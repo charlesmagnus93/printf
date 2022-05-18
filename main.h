d287cbce82975ec4bd765e88d22dbca345a1a7ea
charlemagne <agbolossouc@gmail.com>
2022-05-18 13:17:09 -0700

    printf 0

--- main.h
+++ main.h
@@ -6,6 +6,13 @@
 
 int _printf(const char *format, ...);
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
 typedef struct flags
 {
 	int plus;
@@ -13,6 +20,12 @@ typedef struct flags
 	int hash;
 } flags_t;
 
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
 typedef struct printHandler
 {
 	char c;
